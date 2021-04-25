aea0c0b635b2e9585a5fb0dfa0143fb40b31b396
Sohaib Ahmed Abbasi <34452073+Sohaib-50@users.noreply.github.com>
2021-04-26 03:56:13 +0500

    Time: 28 ms (74.41%), Space: 14.3 MB (37.66%) - LeetHub

--- to-lower-case/to-lower-case.py
+++ to-lower-case/to-lower-case.py
@@ -1,8 +1,20 @@
 class Solution:
+    ## O(n) time, O(n) space?
     def toLowerCase(self, str: str) -> str:
+        str = list(str)
         
         for i in range(len(str)):
             if ord(str[i]) in range(65, 91):
-                str = str[:i] + chr(ord(str[i]) + 32) + str[i+1:]
+                str[i] = chr(ord(str[i]) + 32)
                 
-        return str
+        return "".join(str)
+    
+    
+    ## O(n^2) time, O(1) space
+#     def toLowerCase(self, str: str) -> str:
+        
+#         for i in range(len(str)):
+#             if ord(str[i]) in range(65, 91):
+#                 str = str[:i] + chr(ord(str[i]) + 32) + str[i+1:]
+                
+#         return str
