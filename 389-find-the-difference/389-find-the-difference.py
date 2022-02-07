829536190fc13b4b3aeb020d2b6bcea6e0510520
Sohaib Ahmed Abbasi <34452073+Sohaib-50@users.noreply.github.com>
2022-02-07 21:32:12 +0500

    Time: 86 ms (5.26%), Space: 13.8 MB (96.95%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,15 +1,11 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        charmap = [0 for _ in range(26)]
-        
-        for char in t:
-            charmap[ord(char) - ord("a")] += 1
+        x = 0
         
         for char in s:
-            charmap[ord(char) - ord("a")] -= 1
+            x ^= ord(char)
         
-        for i in range(len(charmap)):
-            if charmap[i] == 1:
-                return chr(i + 97)
-        
+        for char in t:
+            x ^= ord(char)
             
+        return chr(x)
