23c0045cdd686e497d7995c890c20e4c5881ccd1
Sohaib Ahmed Abbasi <34452073+Sohaib-50@users.noreply.github.com>
2022-03-02 19:18:51 +0500

    Time: 64 ms (45.12%), Space: 15.1 MB (55.21%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,14 +1,12 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         ans = [None for _ in range(n)]
-        
+        mappings = {3: "Fizz", 5: "Buzz"}
         for i in range(1, n+1):
-            x = ""
-            if i % 3 == 0:
-                x += "Fizz"
-            if i % 5 == 0:
-                x += "Buzz"
-            
-            ans[i - 1] = x if x != "" else str(i)
+            output = ""
+            for num in mappings:
+                if i % num == 0:
+                    output += mappings[num]
+            ans[i - 1] = output if (output != "") else str(i)
         
         return ans
