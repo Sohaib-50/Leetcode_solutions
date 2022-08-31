806ee636ed1200c094b6730d6daa4e386559fdd7
Sohaib Ahmed Abbasi <34452073+Sohaib-50@users.noreply.github.com>
2022-08-31 18:29:03 +0500

    Update 1-two-sum.py

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,45 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {nums[j]: j for j in range(len(nums))}
+        
         for i in range(len(nums)):
-            for j in range(i +1, len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    return (i, j)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # for i in range(len(nums)):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return i, j
-        
-        
-#     nums_dict = {}
-#         for i in range(len(nums)):
-#             current_number = nums[i]
-#             nums_dict[current_number] = i
-            
-#         for i in range(len(nums)):
-#             current_number = nums[i]
-#             if (target - current_number) in nums_dict:
-#                 return i, nums_dict[target - current_number]
-        
-        
-#         nums_dict = {}
-#         for i in range(len(nums)):
-#             current = nums[i]
-#             if (target - current) in nums_dict:
-#                 return i, nums_dict[target - current]
-                
-#             nums_dict[current] = i
+            required = target - nums[i]
+            if required in indices and i != indices[required]:
+                return (i, indices[target - nums[i]])
