cef21f24ad24bd74a47421c0d0e6f83a66d5605f
Sohaib Ahmed Abbasi <34452073+Sohaib-50@users.noreply.github.com>
2022-09-10 14:15:57 +0500

    Time: 48 ms (54.17%), Space: 13.9 MB (73.12%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.py
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.py
@@ -1,18 +1,35 @@
 class Solution:
     def largestInteger(self, num: int) -> int:
-        nums = [int(i) for i in str(num)]
+        frequency = [0 for i in range(10)]
+        even_idx = 0
+        odd_idx = 1
+        answer = 0
         
-        even_nums = [i for i in nums if i % 2 == 0]
-        even_nums.sort()
-        
-        odd_nums = [i for i in nums if i % 2 != 0]
-        odd_nums.sort()
-        
-        answer = []
-        for i in nums:
-            if i % 2 == 0:
-                answer.append(even_nums.pop())
+        i = 0
+        while (num // (10 ** i) != 0):
+            frequency[(num // (10 ** i)) % 10] += 1
+            i += 1
+            
+        i = 0
+        while (num // (10 ** i) != 0):
+            current_digit = (num // (10 ** i)) % 10
+
+            # if current digit in num is even, find largest current even number
+            if current_digit % 2 == 0:
+                while frequency[even_idx] == 0:
+                    even_idx += 2
+                answer += even_idx * (10 ** i)
+                frequency[even_idx] -= 1
+
             else:
-                answer.append(odd_nums.pop())
-        
-        return int("".join([str(c) for c in answer]))
+                while frequency[odd_idx] == 0:
+                    odd_idx += 2
+                answer += odd_idx * (10 ** i)
+                frequency[odd_idx] -= 1
+            
+            i += 1
+
+        return answer
+                
+                
+            
