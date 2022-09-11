e3ef71a069ba916de1885c07262c7201dc3150d1
Sohaib Ahmed Abbasi <34452073+Sohaib-50@users.noreply.github.com>
2022-09-11 14:19:03 +0500

    Time: 3408 ms (7.15%), Space: 31.4 MB (69.38%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,7 +1,5 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        less_thans = []
-        more_thans = []
         pivot_frequency = 0
         ans = []
         
@@ -9,18 +7,13 @@ class Solution:
             if i == pivot:
                 pivot_frequency += 1
             elif i < pivot:
-                less_thans.append(i)
-            else:
-                more_thans.append(i)
-                
-                
-        for i in less_thans:
-            ans.append(i)
-        
+                ans.append(i)
+
         for _ in range(pivot_frequency):
             ans.append(pivot)
         
-        for i in more_thans:
-            ans.append(i)
+        for i in nums:
+            if i > pivot:
+                ans.append(i)
             
         return ans
