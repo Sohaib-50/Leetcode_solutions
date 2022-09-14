50dd8d1e02952d22b3a08dde05fbc1b7a0841e11
Sohaib Ahmed Abbasi <34452073+Sohaib-50@users.noreply.github.com>
2022-09-14 21:03:05 +0500

    Update 1-two-sum.py

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        indices = {nums[j]: j for j in range(len(nums))}
+        indices_map = {nums[j]: j for j in range(len(nums))}
         
         for i in range(len(nums)):
             required = target - nums[i]
-            if required in indices and i != indices[required]:
-                return (i, indices[target - nums[i]])
+            if required in indices_map and i != indices_map[required]:
+                return (i, indices_map[target - nums[i]])
