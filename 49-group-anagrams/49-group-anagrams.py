e6f75dd22ebb7cee051e3486b71d0ae2ee028491
Sohaib Ahmed Abbasi <SohaibAhmedAbbasi0@gmail.com>
2022-10-03 19:08:17 +0500

    Time: 234 ms (26.31%), Space: 18 MB (54.08%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,15 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = {}
-        for i, str in enumerate(strs):
-            sorted_str = tuple(sorted(str))
+        for s in strs:
+            sorted_str = tuple(sorted(s))
             if sorted_str in hashmap:
-                hashmap[sorted_str].append(i)
+                hashmap[sorted_str].append(s)
             else:
-                hashmap[sorted_str] = [i]
+                hashmap[sorted_str] = [s]
                 
         answer = []
         for key in hashmap:
-            answer.append([strs[i] for i in hashmap[key]])
+            answer.append(hashmap[key])
             
         return answer
