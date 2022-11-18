aed603ec22c869c04cf133ccfb98e97f0b81b744
Sohaib Ahmed Abbasi <SohaibAhmedAbbasi0@gmail.com>
2022-11-18 18:34:02 +0500

    Time: 289 ms (53.72%), Space: 14.8 MB (53.60%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,30 +3,48 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        zero_positions = {(x, y) for x in range(len(matrix)) for y in range(len(matrix[0])) if matrix[x][y] == 0}
+        zero_rows = set()
+        zero_columns = set()
+        for x in range(len(matrix)):
+            for y in range(len(matrix[0])):
+                if matrix[x][y] == 0:
+                    zero_rows.add(x)
+                    zero_columns.add(y)
+                    
+        for row in zero_rows:
+            col = 0
+            while col < len(matrix[0]):
+                matrix[row][col] = 0
+                col += 1
         
-        for x, y in zero_positions:
+        for col in zero_columns:
+            row = 0
+            while row < len(matrix):
+                matrix[row][col] = 0
+                row += 1
+        
+#         for x, y in zero_positions:
             
-            # go up
-            temp_x = x - 1
-            while temp_x >= 0:
-                matrix[temp_x][y] = 0
-                temp_x -= 1
+#             # go up
+#             temp_x = x - 1
+#             while temp_x >= 0:
+#                 matrix[temp_x][y] = 0
+#                 temp_x -= 1
                 
-            # go down
-            temp_x = x + 1
-            while temp_x < len(matrix):
-                matrix[temp_x][y] = 0
-                temp_x += 1
+#             # go down
+#             temp_x = x + 1
+#             while temp_x < len(matrix):
+#                 matrix[temp_x][y] = 0
+#                 temp_x += 1
                 
-            # go left
-            temp_y = y - 1
-            while temp_y >= 0:
-                matrix[x][temp_y] = 0
-                temp_y -= 1
+#             # go left
+#             temp_y = y - 1
+#             while temp_y >= 0:
+#                 matrix[x][temp_y] = 0
+#                 temp_y -= 1
                 
-            # go right
-            temp_y = y + 1
-            while temp_y < len(matrix[0]):
-                matrix[x][temp_y] = 0
-                temp_y += 1
+#             # go right
+#             temp_y = y + 1
+#             while temp_y < len(matrix[0]):
+#                 matrix[x][temp_y] = 0
+#                 temp_y += 1
