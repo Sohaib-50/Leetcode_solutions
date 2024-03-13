b6dce6d0872e4b3212b0ae3bc7608981061c0f6a
Sohaib Ahmed Abbasi <sohaibahmedabbasi2@gmail.com>
2024-03-14 01:19:47 +0500

    Update merge-strings-alternately.py

--- merge-strings-alternately/merge-strings-alternately.py
+++ merge-strings-alternately/merge-strings-alternately.py
@@ -1,22 +1,20 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        ans = []
-        i = j = 0
-        current = 1
-        while (i < len(word1) and j < len(word2)):
-            if current == 1:
-                ans.append(word1[i])
-                i += 1
-            else:
-                ans.append(word2[j])
-                j += 1
-                
-            current *= -1
-        
-        if i == len(word1):
-            ans.append(word2[j:])  
-        elif j == len(word2):
-            ans.append(word1[i:])
-            
-        return "".join(ans)
-                
+        ans = ""
+
+        # Add characters from both words one by one till one or both run out of characters
+        p1 = p2 = 0
+        while p1 < len(word1) and p2 < len(word2):
+            ans += word1[p1]
+            ans += word2[p2]
+            p1 += 1
+            p2 += 1
+
+        # add remaining part of the word that has chars left
+        if p1 == len(word1):
+            ans += word2[p2:]
+        elif p2 == len(word2):
+            ans += word1[p1:]
+
+        return ans
+
