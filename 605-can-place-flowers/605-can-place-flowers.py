176d02e7062d01897f9fc285f0d2984c6540c42b
Sohaib Ahmed Abbasi <sohaibahmedabbasi2@gmail.com>
2024-03-21 02:19:58 +0500

    Update 605-can-place-flowers.py

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -1,27 +1,22 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        if n == 0:
-            return True
-        
-        l = len(flowerbed)
-        
-        if l == 1:
-            return flowerbed[0] == 0
-        
-        i = 0
-        while (i < l) and (n > 0):
-            if flowerbed[i] == 0:
-                if i == 0:
-                    if flowerbed[i + 1] == 0:
-                        n -= 1
-                        i += 1
-                elif i == l - 1:
-                    if flowerbed[i - 1] == 0:
-                        n -= 1
-                        i += 1
-                elif (flowerbed[i + 1] == 0) and (flowerbed[i - 1] == 0):
+        a = -1
+        b = 0
+        c = 1
+
+        while b < len(flowerbed) and n > 0:
+            if flowerbed[b] == 1:
+                b += 2
+            else:
+                checks = [flowerbed[b]]
+                if (b - 1) >= 0:
+                    checks.append(flowerbed[b - 1])
+                if (b + 1) < len(flowerbed):
+                    checks.append(flowerbed[b + 1])
+                
+                if all((check == 0 for check in checks)):
                     n -= 1
-                    i += 1
-            i += 1
+                    b += 2
+                else:
+                    b += 1
         return n == 0
-                    
