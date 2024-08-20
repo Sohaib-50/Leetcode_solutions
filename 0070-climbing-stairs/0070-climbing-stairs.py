9348b3d7c8a58c6db17ff2e8bfd1b816973e2b01
Sohaib Ahmed Abbasi <sohaibahmedabbasi2@gmail.com>
2024-08-20 09:41:28 +0500

    Update 0070-climbing-stairs.py

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,8 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        memo = {}
-        def dfs(steps_sofar):
-            if steps_sofar in memo:
-                return memo[steps_sofar]
-            if steps_sofar == n:
-                memo[steps_sofar] = 1
-            elif steps_sofar > n:
-                memo[steps_sofar] = 0
-            else:
-                memo[steps_sofar] = dfs(steps_sofar + 1) + dfs(steps_sofar + 2)
-            return memo[steps_sofar]
-            
-        return dfs(1) + dfs(2)  # no. of routes if we take 1 step + no. of routes if we take 2 steps
+    def climbStairs(self, n: int, memo={1: 1, 2: 2}) -> int:
+
+        # reverse base case
+        if n not in memo:
+            memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        
+        return memo[n]
