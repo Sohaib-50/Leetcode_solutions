dff0c2507c3883fc455b2148c31a685d5dd02485
Sohaib Ahmed Abbasi <sohaibahmedabbasi2@gmail.com>
2025-04-22 16:15:29 +0500

    Update 268-missing-number.py

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,3 +1,16 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        return sum(range(len(nums) + 1)) - sum(nums)
+        '''
+        Intuition: Sum up all numbers in range [0, N].
+        Then subtract all nums from nums from that sum, 
+        remaining value will be the missing number.
+        '''
+        
+        x = 0
+        for i in range(len(nums) + 1):
+            x += i
+            
+        for num in nums:
+            x -= num
+
+        return x
