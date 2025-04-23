bb717b8cc724f26838309817a1dc1b9fb859d547
Sohaib Ahmed Abbasi <sohaibahmedabbasi2@gmail.com>
2025-04-23 17:26:32 +0500

    Update 283-move-zeroes.py

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,17 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
-        # find first 0
-        for zero_pos in range(len(nums)):
-            if nums[zero_pos] == 0:
-                break
-        # if no 0's
-        else:
-            return
-
-        for i in range(zero_pos + 1, len(nums)):
-            if nums[i] != 0:
-                nums[i], nums[zero_pos] = nums[zero_pos], nums[i]
-                zero_pos += 1
         
+        # find first 0
+        for i, num in enumerate(nums):
+            if num == 0:
+                break
+        
+        zero_ptr = i
+        for i in range(zero_ptr + 1, len(nums)):
+            print(i, nums[i])
+            if nums[i] != 0:
+                nums[zero_ptr], nums[i] = nums[i], nums[zero_ptr]
+                
+                for j in range(zero_ptr + 1, i):
+                    if nums[j] == 0:
+                        zero_ptr = j
+                        break
+                else:
+                    zero_ptr = i
+            
